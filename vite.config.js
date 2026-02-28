1c177ef5fd48063423eaf31d6858c957c4e33f55
houhouoih <rv504693@gmail.com>
2026-02-28 16:27:47 +0530

    Update Vite config by removing server settings
    Removed server configuration and added base path.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-  
-    allowedHosts: [
-      'bf852b11-61ce-4838-82ad-a0638add2598-00-2mecu2ku9fwp.sisko.replit.dev'
-    ]
-  }
+  base: './',
 })
